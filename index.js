6600a2225aa51411192726c1adeb0d498ba64583
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 16:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function wrapAdjective(parameter="*") {
         return `You are ${parameter}${adjective}${parameter}!`
     }
 }
+let Calculator = {
+    add: function(x,y) {
+        return x+y
+    },
+    subtract: function(x,y) {
+        return x-y
+    },
+    multiply: function(x,y) {
+        return x*y
+    }, 
+    divide: function(x,y) {
+        return x/y
+    }
+}
